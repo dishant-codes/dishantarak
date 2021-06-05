30a28b14f4886eb2f4d3287e8f98089fb68e62a0
dishant-codes <dishantaarak2696@gmail.com>
2021-06-05 10:52:46 +0530

    deploy 2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.min.js";
+import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
